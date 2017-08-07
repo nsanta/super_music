82b1f40f65ed5b0a63b28c0c80b750076b4cedee
Nicolas Santa <nicolas55ar@gmail.com>
2017-08-07 15:33:43 -0300

    add list songs by album and artist

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,7 @@ class Song < ApplicationRecord
 
   belongs_to :album
   belongs_to :artist
+
+  scope :by_album, ->(album_id) { where(album_id: album_id) }
+  scope :by_artist, ->(artist_id) { where(artist_id: artist_id) }
 end
